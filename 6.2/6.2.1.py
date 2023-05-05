b9ca9e2f43d518e2cce021a50466e16602ba4a98
Marat Ospanov <maratospanv@gmail.com>
2023-05-05 22:41:35 +0600

    Add 6.2

---
+++ 6.2/6.2.1.py
@@ -0,0 +1,6 @@
+number = int(input('Введите число: '))
+while number > 0:
+    number2 = int(input('Введите второе число: '))
+    number2 += number
+    print(number2)
+print(number)
---
+++ 6.2/6.2.2.py
@@ -0,0 +1,5 @@
+password = int(input('Введите пароль: '))
+while password != 235:
+    print('Неверный пароль!')
+    password = int(input('Попробуйе еще раз: '))
+print('Пароль верный! Добро пожаловать.')
