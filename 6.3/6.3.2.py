79ffdb91d90e6b6b77bda30df57121d7fb24ef78
Marat Ospanov <maratospanv@gmail.com>
2023-05-06 18:44:40 +0600

    changed 6-3-2

--- 6.3/6.3.2.py
+++ 6.3/6.3.2.py
@@ -2,9 +2,12 @@ number = int(input('Введите число: '))
 summ = 0
 while number != 0:
     last_num = number % 10
+    print(last_num)
     if last_num == 5:
         print('Найдена цифра 5')
+        break
     summ += last_num
     #number // 10
     #number % 10
-print(number)
+    number //= 10
+print('Сумма:', summ)
