fc8552f426fb778653954fb5c9bad29001c3793d
Marat Ospanov <maratospanv@gmail.com>
2023-05-09 20:41:18 +0600

    change task 6

--- practic 6/task_6.py
+++ practic 6/task_6.py
@@ -1,10 +1,21 @@
-x = int(input('Введите число X: '))
-y = int(input('Введите число Y: '))
-p = int(input('Введите число P: '))
-
+x = int(input('Введите число вклада X: '))
+y = int(input('Введите итоговую сумму Y: '))
+p = int(input('Введите число процента P: '))
+#Процент 1 год
+#percent = (x / 100) * p
+#print(percent)
+#summ = x + percent
+#print(summ)
+#2 год
+year = 0
 percent = (x / 100) * p
-print(percent)
 summ = x + percent
-print(int(summ))
+while y > x:
+    summ = ((summ / 100) * p)+summ
+    print(summ)
+    if summ == 465.3985:
+        break
 
-a =  
+##3 год
+#summ = ((summ / 100) * p)+summ
+#print(summ)
