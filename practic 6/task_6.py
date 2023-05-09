d1cbd825bcc76042faab2069d3d7693b7239ff48
Marat Ospanov <maratospanv@gmail.com>
2023-05-09 18:22:30 +0600

    Change task6

---
+++ practic 6/task_6.py
@@ -0,0 +1,8 @@
+x = int(input('Введите число X: '))
+#y = int(input('Введите число Y: '))
+p = int(input('Введите число P: '))
+
+percent = (x / 100) * p
+print(percent)
+summ = x + percent
+print(int(summ))
