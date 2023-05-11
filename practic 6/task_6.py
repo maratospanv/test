a25cbf7f7a54e5fc75371021455f4857a88d5a57
Marat Ospanov <maratospanv@gmail.com>
2023-05-11 21:43:45 +0600

    Take task 6

--- practic 6/task_6.py
+++ practic 6/task_6.py
@@ -1,21 +1,18 @@
-x = int(input('Введите число вклада X: '))
-y = int(input('Введите итоговую сумму Y: '))
-p = int(input('Введите число процента P: '))
-#Процент 1 год
-#percent = (x / 100) * p
-#print(percent)
-#summ = x + percent
-#print(summ)
-#2 год
-year = 0
-percent = (x / 100) * p
-summ = x + percent
-while y > x:
-    summ = ((summ / 100) * p)+summ
-    print(summ)
-    if summ == 465.3985:
-        break
-
-##3 год
-#summ = ((summ / 100) * p)+summ
-#print(summ)
+#days_on_year = 365
+vklad = int(input('Введите сумму вклада: '))
+percent = int(input('Введите процент по вкладу: '))
+plan = int(input('Введите планируемую сумму накопления: '))
+bank = 0
+attempt = 0
+percent = (percent/100)
+while bank < plan:
+    attempt += 1
+    perc_vklad = int((vklad * percent))
+    vklad = int(perc_vklad + vklad)
+    bank = vklad
+if attempt <= 2 or attempt <= 4:
+     print('Понадобиться', attempt, 'года')
+     print('За', attempt, 'года накопите', bank)
+elif attempt <= 5 or attempt <= 19:
+     print('Понадобиться', attempt, 'лет')
+     print('За', attempt, 'лет накопите', bank)
