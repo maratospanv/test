42a8c7a2899ddd2431ea78e7a7debdb73a908a89
Marat Ospanov <maratospanv@gmail.com>
2023-05-11 16:06:30 +0600

    task 8

--- test.py
+++ test.py
@@ -1,27 +1,30 @@
-search_number = int(input('Введите число: '))
+search_number = int(input('Введите искомое число: '))
 attempt = 0
-hint = 0
-number = 50
+min = 1
+max = 100
+avarage = max // 2
 while True:
     attempt = attempt + 1
     if (search_number >= 1) or (search_number <= 100):
-        answer_message = 'Ваше число больше или меньше или равно', str(number)+' ? '
+        answer_message = 'Ваше число больше или меньше или равно', str(avarage)+' ? '
         answer = int(input(answer_message))
         if attempt > 7:
             print('Количесво попыток исчерпано')
             break
-        elif answer == 1:
-            print('Вы нашли загаданное число', search_number)
+        if answer == 1:
+            print('Вы нашли загаданное число', avarage)
+            print('Число попыток:', attempt)
             break
-        elif answer == 2:
-            print('Число больше числа', number)
-            #number = (int(number) * 2) - (number // 2)
-            number = number % 2
-            print(number)
-        elif answer == 3:
-            print('Число меньше числа', number)
-            number = number // 2
-            print(number)
+        if answer == 2:
+            #print('Число больше числа', avarage)
+            min = avarage
+            avarage = (max + avarage) // 2
+        if answer == 3:
+            #print('Число меньше числа', avarage)
+            max = avarage
+            avarage = (min + max) // 2
+
+          
 
 
 
