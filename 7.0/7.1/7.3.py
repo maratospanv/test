a5fdc7753ed07b451b5e7c7d15fcee0ebc7e4fb3
Marat Ospanov <maratospanv@gmail.com>
2023-05-13 11:25:04 +0600

    solved 7.3

--- 7.0/7.1/7.3.py
+++ 7.0/7.1/7.3.py
@@ -1,2 +1,4 @@
 for tickets in 345,19,87,1020,421:
-        print(tickets)
+        #if tickets % 5 == 0:
+        if (99 < tickets and 1000 > tickets) and tickets % 5 == 0:
+                print(tickets)
