0fc5cb6ea8d9ae5145f8583abc14400e2dce8aaf
Marat Ospanov <maratospanv@gmail.com>
2023-05-15 08:22:55 +0600

    test

--- practic 6/task_3.py
+++ practic 6/task_3.py
@@ -1,6 +1,8 @@
 number = int(input('Введите число: '))
 count = 0
-while number > 0:
+if number == 0:
+    count +=1 
+while number >= 0:
     count +=1
     number = number // 10
 print ('Ответ:', count)
