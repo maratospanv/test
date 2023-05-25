5766378719c519f193a35a49cfcfa60e465c4c6b
Marat Ospanov <maratospanv@gmail.com>
2023-05-25 11:20:49 +0600

    task6

--- practic 9/task_6.py
+++ practic 9/task_6.py
@@ -2,15 +2,14 @@ text = input('Ведите символы: ')
 symbol_count = 0
 milk_count = 0
 total_milk_count = 0
+busy = 0
+total = 20
 
 for i in text:
     symbol_count += 1
-    if i == 'a':
+    if i == 'b':
+        busy += 1
         milk_count += 2
-        print(milk_count)
-        milk_count += milk_count
-print('всего', milk_count)
-    #for total in i:
-    #    total_milk_count += milk_count
         
-#print(total_milk_count)
+print('Будет произведено молока в день:', milk_count, 'л.')
+print('Занято стойл:', busy, 'Свободно стойл:', total - busy)
