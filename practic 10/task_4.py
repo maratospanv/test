973ac3edb13a775cebe4094cf54ad0a0dcf38ebd
Marat Ospanov <maratospanv@gmail.com>
2023-06-08 16:34:15 +0600

    A

--- practic 10/task_4.py
+++ practic 10/task_4.py
@@ -1,12 +1,21 @@
-numSeq = int(input('Введите количество чисел: '))
-print()
+seq_num = int(input('Введите количество чисел: '))
 count = 0
-for num in range(1, numSeq + 1):
+
+for num in range(seq_num):
     number = int(input('Введите число: '))
-    for seq in range(2, num):
-        if num % seq == 0:
-            break
-        else:
+
+    for i in range(1,number):
+        flag=True
+        check_simple = number - 1
+
+        while check_simple > 1:
+            if number % check_simple == 0:
+                flag = False
+                break
+            check_simple -= 1
+
+        if flag:
+            #print(number, '- простое число')
             count += 1
-print()
+            break
 print('Простых чисел в последовательности', count)
