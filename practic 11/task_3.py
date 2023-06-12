91648a916e5bf96c610c6302f10cb37cf3a60ed9
Marat Ospanov <maratospanv@gmail.com>
2023-06-12 22:42:26 +0600

    Task 3-4

--- practic 11/task_3.py
+++ practic 11/task_3.py
@@ -7,14 +7,26 @@ download_size = 0
 second = 0
 download_percent = 0
 
-for i in range(int(size / speed)):
+print()
+
+for i in range(int(size / speed) + 1):
     if size <= 0 or speed <= 0:
         print('Невозможно скачать файл')
         break
-    else:
+    else:    
         download_size += speed
         second += 1
         download_percent = math.ceil(download_size/(size / 100))
-        print('Прошло', second, 'сек. Скачано', download_size, 'из', size, 'Мб', '('+str(download_percent)+'%)')
 
+        if download_size > size:
+             
+            more_size = download_size - size
+            download_size -= more_size
+            more_percent = download_percent - 100
+            download_percent -= more_percent
+            print('Прошло', second, 'сек. Скачано', download_size, 'из', size, 'Мб', '('+str(download_percent)+'%)')
+        else:
+            print('Прошло', second, 'сек. Скачано', download_size, 'из', size, 'Мб', '('+str(download_percent)+'%)')
+
+print()
 print('Скачано', str(download_size)+' Мб,', 'скачивание заняло', second, 'секунд' )
