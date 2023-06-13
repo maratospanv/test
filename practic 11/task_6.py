9477f0b1636a3a95d5e8f00d1dabb79f2ae0f891
Marat Ospanov <maratospanv@gmail.com>
2023-06-13 21:34:05 +0600

    Task 6

--- practic 11/task_6.py
+++ practic 11/task_6.py
@@ -1,28 +1,26 @@
-# x_coord = float(input("По горизонтали: "))
-# y_coord = float(input("По вертикали: "))
-
-# if 0 < x_coord < 0.8 and 0 < y_coord < 0.8:
-#     x_number = int(x_coord * 10)
-#     y_number = int(y_coord * 10)
-#     print("Фигура находится в клетке (", x_number, ",", y_number, ")")
-#     # center_x = x_number / 10 + 0.05
-#     # center_y = y_number / 10 + 0.05
-#     # delta_x = round(center_x - x_coord, 3)
-#     # delta_y = round(center_y - y_coord, 3)
-#     # print("Поправьте положение фигуры на (", delta_x, ",", delta_y, ")")
-# else:
-#     print("Клетки с такой координатой не существует")
-
-
 print('Введите местоположение коня: ')
+print()
 
-start_x = float(input())
-start_y = float(input())
+start_x = float(input('Введите начальную позицию по горизонтали: '))
+start_y = float(input('Введите начальную позицию по вертикали: '))
 
-#end_x = float(input())
-#end_y = float(input())
+end_x = float(input('Введите конечную позицию по горизонтали: '))
+end_y = float(input('Введите конечную позицию по вертикали: '))
 
 start_x_num = int(start_x * 10)
 start_y_num = int(start_y * 10)
 
-print(start_x_num, start_y_num)
+end_x_num = int(end_x * 10)
+end_y_num = int(end_y * 10)
+
+diff_x = abs(start_x_num - end_x_num)
+diff_y = abs(start_y_num - end_y_num)
+
+print()
+
+if diff_x == 1 and diff_y == 2 or diff_x == 2 and diff_y == 1:
+    print('Конь в клетке end', start_x_num, start_y_num, 'Точка в клетке', end_x_num, end_y_num)
+    print('Да, конь может ходить в эту точку.')
+else:
+    print('Конь в клетке', start_x_num, start_y_num, 'Точка в клетке', end_x_num, end_y_num)
+    print('Нет, конь не может ходить в эту точку.')
