bf1d15bd2b01eac9b7aaa9b8ef683147cc0dcd19
Marat Ospanov <maratospanv@gmail.com>
2023-06-20 23:21:08 +0600

    change task 13 3

--- practic 13/task_3.py
+++ practic 13/task_3.py
@@ -1,3 +1,35 @@
+def contrariwise(num):
+    a = ''
+    if num == 0:
+        print('\nПрограмма завершена!')
+    while num > 0:
+        s = num % 10
+        num //= 10
+        if s >= 0:
+            a += str(s)
+    return a
+
+first_n = int(input("Введите первое число: "))
+second_n = int(input('Введите второе число: '))
+
+
+
+
+
+first_g = (contrariwise(first_n))
+second_g = (contrariwise(second_n))
+print(int(first_g))
+print(int(second_g))
+
+g_sum = int(first_g) + int(second_g)
+
+print(g_sum)
+
+print(contrariwise(g_sum))
+
+
+
+
 # def contrariwise(num):
 #     a = ''
 #     if num == 0:
@@ -29,29 +61,29 @@
 # summ_cont = contrariwise(sum)
 # print('Сумма наоборот:', summ_cont)
 
-def contrariwise(num):
-    a = ''
-    for i in range(1,len(num)+1):
-        a += num[-i]
-    return a
+# def contrariwise(num):
+#     a = ''
+#     for i in range(1,len(num)+1):
+#         a += num[-i]
+#     return a
 
-number1 = input('Введите первое число: ')
-number2 = input('Введите первое число: ')
+# number1 = input('Введите первое число: ')
+# number2 = input('Введите первое число: ')
 
-num_cont1 = (contrariwise(number1))
-num_cont2 = (contrariwise(number2))
+# num_cont1 = (contrariwise(number1))
+# num_cont2 = (contrariwise(number2))
 
-print()
+# print()
 
-print('Первое число наоборот:', num_cont1)
-print('Второе число наоборот:', num_cont2)
+# print('Первое число наоборот:', num_cont1)
+# print('Второе число наоборот:', num_cont2)
 
-summ_cont = int(num_cont1) + int(num_cont2)
+# summ_cont = int(num_cont1) + int(num_cont2)
 
-print()
+# print()
 
-print('Сумма:', summ_cont)
+# print('Сумма:', summ_cont)
 
-print('Сумма наоборот:', contrariwise(str(summ_cont)))
+# print('Сумма наоборот:', contrariwise(str(summ_cont)))
 
 
