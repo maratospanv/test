bf8587a1cb296ebf826bdc72f59cfcbb89b32e1a
Marat Ospanov <maratospanv@gmail.com>
2023-06-26 13:08:15 +0600

    inux practic text 51

--- 2.Linux/rename.sh
+++ 2.Linux/rename.sh
@@ -1,20 +1,18 @@
-#!/bin/bash
-
 if [ $# -ne 2 ]
 then
-	echo "Missing arguments, need 2 arguments!"
-	exit 1
+        echo "Missing arguments, need 2 arguments!"
+        exit 1
 fi
 
 if [ ! -f "$1" ]
 then
-	echo "File $1 is a directory"
-	exit 1
+        echo "File $1 is a directory"
+        exit 1
 fi
 
 if  cp "$1" "$2" && rm -f "$1"
 then
-	echo "File $1 renamed to $2"
+        echo "File $1 renamed to $2"
 else
-	echo "File $1 NOT renamed to $2"
+        echo "File $1 NOT renamed to $2"
 fi
