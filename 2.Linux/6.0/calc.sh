9142f3081ae54d9dca53d2caa25ae281faeb8491
Marat Ospanov <maratospanv@gmail.com>
2023-06-27 16:14:03 +0600

    practic 6 1

--- 2.Linux/6.0/calc.sh
+++ 2.Linux/6.0/calc.sh
@@ -4,16 +4,11 @@ read -p "enter first number: " x
 read -p "enter second number: " y
 read -p "enter action symbol: " operator
 
-case $operator in
+case "$operator" in
 "+") echo "$x + $y =" $(expr "$x" + "$y");;
 "-") echo "$x - $y =" $(expr "$x" - "$y");;
 "*") echo "$x + $y =" $(expr "$x" \* "$y");;
 "**") echo "$x + $y =" $(expr "$x" + "$y");;
-"/")echo "$x / $y =" $(expr "$x" / "$y")
-if [ "$y" -eq 0 ]
-then
-    echo "Error"
-    exit 1
-;;
+"/")if [ "$y" -eq 0 ]; then echo "Error"; else echo "$x / $y =" $(expr "$x" / "$y"); fi;;
 *) echo "Unknown operation"
 esac
