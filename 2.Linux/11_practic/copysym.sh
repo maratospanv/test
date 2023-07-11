a43df02b89c8469cf15212ddb92031243a171738
Marat Ospanov <maratospanv@gmail.com>
2023-07-11 15:53:32 +0600

    Practic 11

---
+++ 2.Linux/11_practic/copysym.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+read -p "Enter file or directory name for copy: " symlink
+read -p "Enter new file name to copy: " fromsymlink
+fsymlink="symbolic link"
+symname=`stat $symlink | grep Size | awk '{print $8,$9}'`
+
+if [ "$symname" = "$fsymlink" ]
+then
+	cp $symlink $fromsymlink
+	if [ "`test -f $fromsymlink && echo "$fromsymlink exists." || echo "$fromsymlink not exists." | echo $?`" = "$fromsymlink exists." ]
+	then
+		rm -f $symlink
+		mv $fromsymlink $symlink
+	fi
+else
+	echo "File is not symlink"
+fi
+
