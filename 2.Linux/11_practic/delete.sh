bcf08159c69f145c3cec695295be776a94d6160a
Marat Ospanov <maratospanv@gmail.com>
2023-07-11 14:37:35 +0600

    Start practic 11

---
+++ 2.Linux/11_practic/delete.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Find and delete
+find TRUSH/ -name "*" -mtime +2 -exec rm -f {} \;
+# Input filename
+read -p "Enter file or directory name: " filename
+# Archive and move file
+symlink="symbolic link"
+symname=`stat $filename| grep Size | awk '{print $8,$9}'`
+finide=`ls -li $filename | awk '{print $1}'`
+
+if [ "$symname" = "$symlink" ]
+then
+	rm -f $filename
+	echo "Symlynk deleted for file $filename"
+
+elif [ `ls -li $filename | awk '{print $3}'` -ne '1' ]
+then
+	tar --remove-files -czf $filename.tar.gz $filename && mv $filename.tar.gz TRUSH/
+else
+tar --remove-files -czf $filename.tar.gz $filename && mv $filename.tar.gz TRUSH/
+fi
