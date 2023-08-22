951073623517b203e1a6bb8d917a319b4bbd6b71
Marat Ospanov <maratospanv@gmail.com>
2023-08-22 23:13:18 +0600

    final 6

--- 2.Linux/final_work/gcloud_install.sh
+++ 2.Linux/final_work/gcloud_install.sh
@@ -68,10 +68,10 @@ echo 'set_var EASYRSA_DIGEST sha512' >> vars && \
 sudo sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf && sudo sysctl -p && \ 
 sudo sed -i 's/DEFAULT_FORWARD_POLICY="DROP"/DEFAULT_FORWARD_POLICY="ACCEPT"/g' /etc/default/ufw && \ 
 sudo cp ~/test/2.Linux/final_work/before.rules /etc/ufw/before.rules && \
-sudo ufw enable && sudo ufw allow 1194/udp && sudo ufw allow OpenSSH && echo "y" | sudo ufw disable && echo "y" | sudo ufw enable && \
+sudo ufw --force enable && sudo ufw allow 1194/udp && sudo ufw allow OpenSSH && \
 mkdir -p ~/client-configs/{files,keys} && \
 cp ~/test/2.Linux/final_work/base.conf ~/client-configs/base.conf && \
-cp ~/test/2.Linux/final_work/make_config.sh ~/client-configs/ && \
+cp ~/test/2.Linux/final_work/make_config.sh ~/client-configs/ && chmod +x ~/client-configs/make_config.sh && \
 sudo cp ~/test/2.Linux/final_work/{ca.crt,ta.key} ~/client-configs/keys/ && \
 myip=`dig @resolver4.opendns.com myip.opendns.com +short` && myport=1194 && \s
 sed -i "s/remote my-server-1 1194/remote $myip $myport/g" ~/client-configs/base.conf && sed -i "s/remote my-server-1 1194/remote $myip $myport/g" ~/client-configs/base.conf && \
