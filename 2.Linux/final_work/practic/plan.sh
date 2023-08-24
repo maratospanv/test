f1bb5fa391933f34c74ab43c3b49df956c0c0a73
Marat Ospanov <maratospanv@gmail.com>
2023-08-24 17:44:44 +0600

    start final10

---
+++ 2.Linux/final_work/practic/plan.sh
@@ -0,0 +1,77 @@
+#1. создать чистый pki сервер
+#!/bin/bash
+gcloud config set project avid-glass-396110
+gcloud compute instances create pki-server \
+    --project=avid-glass-396110 \
+    --zone=us-east1-d \
+    --machine-type=e2-micro \
+    --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default \
+    --maintenance-policy=MIGRATE \
+    --provisioning-model=STANDARD \
+    --service-account=492882342467-compute@developer.gserviceaccount.com \
+    --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append \
+    --create-disk=auto-delete=yes,boot=yes,device-name=instance-1,image=projects/ubuntu-os-cloud/global/images/ubuntu-2204-jammy-v20230727,mode=rw,size=10,type=projects/avid-glass-396110/zones/us-east1-d/diskTypes/pd-balanced \
+    --no-shielded-secure-boot \
+    --shielded-vtpm \
+    --shielded-integrity-monitoring \
+    --labels=goog-ec-src=vm_add-gcloud \
+    --reservation-affinity=any
+sleep 30
+gcloud compute ssh `gcloud compute instances list | grep pki-server | awk '{print $1}'` -- 'sudo apt update && sudo apt-get install -y easy-rsa git prometheus-node-exporter && \
+mkdir ~/easy-rsa && sudo ln -s /usr/share/easy-rsa/* ~/easy-rsa/ && \
+sudo chown `whoami` ~/easy-rsa/* && chmod 700 ~/easy-rsa/* && \
+cd ~/easy-rsa && \
+cp vars.example vars && \
+echo 'set_var EASYRSA_BATCH          "yes"' >> vars && \
+echo 'set_var EASYRSA_ALGO           ec' >> vars && \
+echo 'set_var EASYRSA_DIGEST sha512' >> vars && \
+cd /home/`whoami`/easy-rsa/ && \
+./easyrsa init-pki && \
+cd /home/`whoami`/easy-rsa/ && \
+./easyrsa build-ca'
+#sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g'  && \
+#sudo systemctl restart sshd.service
+#sudo useradd vpn && echo -ne "p@$$w0rD\np@$$w0rD\n" | sudo passwd vpn && sudo usermod -aG sudo vpn && sudo usermod -aG google-sudoers vpn'
+
+#2. создать чистый vpn сервер
+gcloud config set project avid-glass-396110
+gcloud compute instances create vpn-server \
+    --project=avid-glass-396110 \
+    --zone=us-east1-d \
+    --machine-type=e2-micro \
+    --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default \
+    --maintenance-policy=MIGRATE \
+    --provisioning-model=STANDARD \
+    --service-account=492882342467-compute@developer.gserviceaccount.com \
+    --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append \
+    --create-disk=auto-delete=yes,boot=yes,device-name=instance-1,image=projects/ubuntu-os-cloud/global/images/ubuntu-2204-jammy-v20230727,mode=rw,size=10,type=projects/avid-glass-396110/zones/us-east1-d/diskTypes/pd-balanced \
+    --no-shielded-secure-boot \
+    --shielded-vtpm \
+    --shielded-integrity-monitoring \
+    --labels=goog-ec-src=vm_add-gcloud \
+    --reservation-affinity=any
+sleep 30
+gcloud compute firewall-rules create allow-1194 --action=ALLOW --rules=udp:1194 --direction=INGRESS
+gcloud compute ssh `gcloud compute instances list | grep vpn-server | awk '{print $1}'` -- 'sudo apt update && sudo apt-get install -y easy-rsa openvpn git prometheus-node-exporter && \
+#sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g'  && \
+#sudo echo "PermitRootLogin yes" >> /etc/ssh/sshd_config  && \
+#sudo systemctl restart sshd.service && \
+mkdir ~/easy-rsa && sudo ln -s /usr/share/easy-rsa/* ~/easy-rsa/ && \
+sudo chown `whoami` ~/easy-rsa/* && chmod 700 ~/easy-rsa/* && \
+cd ~/easy-rsa && \
+cp vars.example vars && \
+echo 'set_var EASYRSA_ALGO           ec' >> vars && \
+echo 'set_var EASYRSA_DIGEST sha512' >> vars && \
+cd /home/`whoami`/easy-rsa/ && \
+./easyrsa init-pki && \
+cd /home/`whoami`/easy-rsa/ && \
+echo -ne "\n" | ./easyrsa gen-req vpn nopass && \
+cd ~ && git clone https://github.com/maratospanv/test.git'
+gcloud compute scp vpn-server:~/easy-rsa/pki/reqs/vpn.req ~ && \
+gcloud compute scp ~/vpn.req pki-server:~/ 
+gcloud compute ssh `gcloud compute instances list | grep pki-server | awk '{print $1}'` -- 'cd cd /home/`whoami`/easy-rsa/ && \ && \
+./easyrsa import-req ~/vpn.req server && \
+cp ~/vpn.req /home/`whoami`/easy-rsa/pki/reqs && \
+cd /home/`whoami`/easy-rsa/ && \ && \
+./easyrsa sign-req server vpn'
+#gcloud compute ssh `gcloud compute instances list | grep vpn-server | awk '{print $1}'` -- 'bash /home/`whoami`/test/2.Linux/final_work/vpn.sh'
