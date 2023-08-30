6745cd17545b3c80c09f8cbb2ac60e0c16a3b0c8
Marat Ospanov <maratospanv@gmail.com>
2023-08-30 20:53:41 +0600

    final 15

--- 2.Linux/final_work/mon.sh
+++ 2.Linux/final_work/mon.sh
@@ -12,3 +12,39 @@ cat <<EOF >> /etc/prometheus/prometheus.yml
     static_configs:
       - targets: ['vpn-server:9100']
 EOF
+
+
+
+groups:
+    - name: _alerts
+      rules:
+      - alert: HighCPUtilization
+        expr: rate(node_cpu_seconds_total{mode="system"}[2m]) > 0.2
+        for: 2m
+        labels:
+          severity: critical
+        annotations:
+          summary: High CPU utilization on host {{ $labels.instance }}
+          description: The CPU utilization on host {{ $labels.instance }}.
+
+      - alert: HighMemoryUtilization
+        expr: node_memory_MemTotal_bytes - node_memory_MemFree_bytes - (node_memory_Buffers_bytes + node_memory_Cached_bytes) == node_memory_MemTotal_bytes - (node_memory_MemTotal_bytes/100*85)
+        for: 2m
+        labels:
+          severity: critical
+        annotations:
+          summary: High Memory utilization on host {{ $labels.instance }}
+          description: The Memory utilization on host {{ $labels.instance }} has exceeded 85% for 2 minutes.
+
+      - alert: HighDiskUtilization
+        expr: ((node_filesystem_size_bytes{mountpoint="/"}) - (node_filesystem_free_bytes{mountpoint="/"} ))  / ((node_filesystem_size_bytes{mountpoint="/"}) / 100 ) > 85
+        for: 5m
+        labels:
+          severity: critical
+        annotations:
+          summary: High Disk utilization on host {{ $labels.instance }}
+          description: The Disk utilization on host {{ $labels.instance }} has exceeded 85% for 5 minutes.
+
+
+          rate(node_network_receive_errs_total[5m]) OR rate(node_network_transmit_errs_total[5m]) = 100
+
