32930f5d6e27d455671775c239e252798c17d0c7
Marat Ospanov <maratospanv@gmail.com>
2023-09-10 16:56:32 +0600

    finished

---
+++ 2.Linux/final_work/finished/vpn.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+oldiface=eth0
+iface=`ip a | grep '2: ' | awk '{print $2}' | sed 's/://g'`
+sudo sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf &> /dev/null && sudo sysctl -p &> /dev/null && \ 
+sudo sed -i 's/DEFAULT_FORWARD_POLICY="DROP"/DEFAULT_FORWARD_POLICY="ACCEPT"/g' /etc/default/ufw &> /dev/null && \ 
+sudo cp ~/devops-engineer-basics/before.rules /etc/ufw/before.rules && \
+sudo ufw --force enable && sudo ufw allow 1194/udp && sudo ufw allow OpenSSH && sudo ufw allow 9100 && \
+mkdir -p ~/client-configs/{files,keys} && \
+cp ~/devops-engineer-basics/base.conf ~/client-configs/base.conf && \
+cp ~/devops-engineer-basics/make_config.sh ~/client-configs/ && chmod +x ~/client-configs/make_config.sh && \
+sudo cp ~/certs/{ca.crt,ta.key} ~/client-configs/keys/ && \
+myip=`dig @resolver4.opendns.com myip.opendns.com +short` && myport=1194 && \
+sed -i "s/remote my-server-1 1194/remote $myip $myport/g" ~/client-configs/base.conf && sed -i "s/remote my-server-1 1194/remote $myip $myport/g" ~/client-configs/base.conf && \
+sudo sed -i "s/$oldiface/$iface/g" /etc/ufw/before.rules && \
+sudo cp ~/certs/{ca.crt,vpn.crt,ta.key,vpn.key} /etc/openvpn/server/ && sudo cp ~/devops-engineer-basics/server.conf /etc/openvpn/server/ && sudo chown root:root /etc/openvpn/server/{ca.crt,vpn.crt,ta.key,vpn.key,server.conf} && \
+sudo systemctl -f enable openvpn-server@server.service && sudo systemctl restart openvpn-server@server.service
